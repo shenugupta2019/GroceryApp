6d88d27601f23e46b9768796bc886019cdd934d2
SHENU GUPTA <shenu.gupta2009@gmail.com>
2023-11-16 11:40:23 +0530

    added home screen files

---
+++ GroceryApp/HomeScreen/Home-styles.ts
@@ -0,0 +1,21 @@
+import React from 'react';
+import {StyleSheet} from 'react-native';
+
+export const homeStyle = StyleSheet.create({
+
+  input:{
+    margin:'30%',
+    borderWidth:2,
+    borderColor:'blue',
+    height:50,
+    paddingHorizontal: 20
+  },
+  button:{
+    margin:30,
+    color:"#841584",
+    accessibilityLabel:"Learn more about this purple button",
+  
+  }
+});
+
+export default homeStyle;
