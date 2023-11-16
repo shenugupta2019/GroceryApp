fab57e67d594113c8f4e5583ecc1f1e2bd468f87
SHENU GUPTA <shenu.gupta2009@gmail.com>
2023-11-16 09:39:14 +0530

    .gitignore is now working

---
+++ GroceryApp/HomeScreen/Home.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export const Home = () => {
+    return (
+      <>
+        <Text>Some text</Text>
+        <View>
+          <Text>Some more text</Text>
+        </View>
+        </>
+    );
+  };
+  
+  export default Home;
